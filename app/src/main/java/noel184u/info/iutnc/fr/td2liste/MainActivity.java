c79daa0863988c7d743dd91cbb209515668badc5
Jimmy Noel <jimmy.noel3@etu.univ-lorraine.fr>
2018-02-12 10:57:03 +0100

    Commit initial

---
+++ app/src/main/java/noel184u/info/iutnc/fr/td2liste/MainActivity.java
@@ -0,0 +1,13 @@
+package noel184u.info.iutnc.fr.td2liste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
